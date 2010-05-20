bcdb85eab0fc4aa2daa3e160cfe9c3d872bd23e9
Peter Kordel <pkordel@gmail.com>
2010-05-20 10:43:54 +0200

    show site options

--- vendor/extensions/site/site_extension.rb
+++ vendor/extensions/site/site_extension.rb
@@ -14,10 +14,25 @@ class SiteExtension < Spree::Extension
   
   def activate
 
-    # uncomment this to enable automatic payment capture
-    # if Spree::Config.instance
-    #   Spree::Config.set(:auto_capture => true)
-    # end
+    # to enable automatic payment capture
+    if Spree::Config.instance
+      Spree::Config.set(:auto_capture => true)
+    end
+    
+    # customize some paypal site options
+    # to have a custom logo appear on paypal, set Spree::Config[:site_url] to a domain you can access
+    # and prepare a logo. see :header_image below
+    # use https to avoid browser warnings about insecure page content
+    Spree::PaypalExpress.class_eval do   
+      def paypal_site_opts
+        { :description             => "Goods from #{Spree::Config[:site_name]}", # site details...
+          :header_image            => "https://" + Spree::Config[:site_url] + "/images/logo.gif", 
+          :background_color        => "ffffff",  # must be hex only, six chars
+          :header_background_color => "ffffff",
+          :header_border_color     => "ffffff"
+        }
+      end
+    end
     
   end
   
